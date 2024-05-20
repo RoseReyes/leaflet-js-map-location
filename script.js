33913d2064d54f7890c12cfe124238960f3d4d39
RoseReyes <roseannlorinreyes@gmail.com>
2024-05-20 12:41:48 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+const map = L.map('map').setView([0, 0], 2);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(map);
+
+const marker = L.marker([0, 0]).addTo(map);
+
+navigator.geolocation.getCurrentPosition((position) => {
+  const lat = position.coords.latitude;
+  const lng = position.coords.longitude;
+
+  marker.setLatLng([lat, lng]).update();
+  map.setView([lat, lng], 13);
+
+  marker.bindPopup(`<strong>Hello World</strong><br> This is my location`);
+});
